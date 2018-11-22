683f873b8c8a7f8f43f6722f8c9688168f504cac
dariusstrasel <strasel.darius@gmail.com>
2018-11-21 21:56:50 -0500

    Add helloworld for api console application.

---
+++ RescueTimeApiClient/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RescueTimeWebApiClient
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
