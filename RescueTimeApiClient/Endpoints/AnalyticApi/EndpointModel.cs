c4ccf844123bb4a463e1c98b883440c8d6cbe9f4
dariusstrasel <strasel.darius@gmail.com>
2018-11-25 20:14:41 -0500

    Move ApiKey into system environmental variable. (oops)

--- RescueTimeApiClient/Endpoints/AnalyticApi/EndpointModel.cs
+++ RescueTimeApiClient/Endpoints/AnalyticApi/EndpointModel.cs
@@ -5,6 +5,6 @@ namespace RescueTimeWebApiClient.Endpoints.AnalyticApi
     public static class EndpointModel
     {
         public static readonly Uri Uri = new Uri("https://www.rescuetime.com/anapi/data");
-        public static readonly string ApiKey = "B63SXZlMIDXc8vGcPozqn7BQ8ciseUzVUEg7XJDB";
+        public static readonly string ApiKey = Environment.GetEnvironmentVariable("RescueTimeApiKey");
     }
 }
